59423de45f3b1d9f60324e9fb573af74989e57a8
tmorales3 <tmorales3@L11712042775.wctc.msft>
2017-01-25 15:32:37 -0600

    Modified main method again

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -15,7 +15,8 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Git");
+        System.out.println("Hello");
     }
     
 }
